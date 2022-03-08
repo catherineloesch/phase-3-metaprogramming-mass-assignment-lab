f6e12d613670c7334d8bb66cb26b922442c8f9c8
Catherine Loesch <catherine.loesch@protonmail.com>
2022-03-08 00:59:26 +0000

    refactoring

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,12 @@
 class Person
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.class.attr_accessor(key)
-      self.send("#{key}=", value)
+  
+  def initialize(attributes_hash=nil)
+    if attributes_hash != nil
+      attributes_hash.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
     end
   end
+  
 end
